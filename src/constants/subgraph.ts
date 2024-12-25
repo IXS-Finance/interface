4a20521b8bd190b2c401ec3abfabc98ddec268bd
Lê Viết Hưng <hung@ixswap.io>
2024-12-25 09:46:22 +0700

    feat: [IXSPD1-2227] add liquidity pool page (#3015)
    * feat: [IXSPD1-2227] add liquidity pool page
    
    * feat: [IXSPD1-2228] Liquidity pool - Integrate with data from Subgraph
    
    * fix: mixing ordering and pagination problem
    
    * fix: handle loading for pool list

--- src/constants/subgraph.ts
+++ src/constants/subgraph.ts
@@ -1,5 +1,9 @@
+export enum SUBGRAPH_QUERY {
+  LBP,
+  POOLS,
+}
 export const SUBGRAPH_URLS: Record<string, Record<number, string>> = {
-  LBP: {
+  [SUBGRAPH_QUERY.LBP]: {
     [137]: 'https://subgraph.satsuma-prod.com/788670ba78ee/ixswap/lbp-subgraph-polygon/api',
     [80002]: 'https://api.studio.thegraph.com/proxy/71824/ixs-lbp-subgraph-test/version/latest',
     [84532]: 'https://subgraph.satsuma-prod.com/788670ba78ee/ixswap/ixs-lbp-base-sepolia/api',
@@ -11,4 +15,7 @@ export const SUBGRAPH_URLS: Record<string, Record<number, string>> = {
     [43113]: 'https://api.studio.thegraph.com/query/27946/earnv-2/version/latest',
     [43114]: 'https://subgraph.satsuma-prod.com/788670ba78ee/ixswap/hycb-avax/api',
   },
+  [SUBGRAPH_QUERY.POOLS]: {
+    84532: 'https://api.studio.thegraph.com/query/80624/balancer-sepolia/version/latest',
+  }
 }
