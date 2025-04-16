ba02fa8e991a0f495faf827fdb97f3eeae1a3796
leonthuongto <leon@ixswap.io>
2025-04-16 20:47:50 +0700

    Change to new contract address

--- src/services/balancer/contracts/voter.ts
+++ src/services/balancer/contracts/voter.ts
@@ -1,12 +1,11 @@
 import { BigNumber } from '@ethersproject/bignumber'
-import { JsonRpcProvider, JsonRpcSigner, TransactionResponse } from '@ethersproject/providers'
+import { TransactionResponse } from '@ethersproject/providers'
 
 import voterAbi from 'abis/voterABI.json'
 import { configService } from 'services/config/config.service'
 import { rpcProviderService } from 'services/rpc-provider/rpc-provider.service'
 import { walletService as walletServiceInstance } from 'services/web3/wallet.service'
 import { EthersContract, getEthersContract } from 'dependencies/EthersContract'
-import { IXVOTER_ADDRESS } from 'constants/addresses'
 import { TransactionBuilder } from 'services/web3/transactions/transaction.builder'
 import { getEthersSigner } from 'hooks/useEthersProvider'
 import { wagmiConfig } from 'components/Web3Provider'
@@ -30,7 +29,7 @@ export class Voter {
   ) {
     const Contract = getEthersContract()
     // @ts-ignore
-    this.instance = new Contract(IXVOTER_ADDRESS[config.network.key], this.abi, this.provider)
+    this.instance = new Contract(configService.network.addresses.voter, this.abi, this.provider)
   }
 
   async getTransactionBuilder(): Promise<TransactionBuilder> {
@@ -39,12 +38,11 @@ export class Voter {
     return new TransactionBuilder(signer)
   }
 
-
   async vote(tokenId: any, poolVote: any, weights: any): Promise<TransactionResponse> {
     const txBuilder = await this.getTransactionBuilder()
 
     const res = await txBuilder.contract.sendTransaction({
-      contractAddress: IXVOTER_ADDRESS[Number(this.config.network.key)],
+      contractAddress: configService.network.addresses.voter,
       abi: this.abi,
       action: 'vote',
       params: [tokenId, poolVote, weights],
