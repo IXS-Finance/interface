355750676ac42c5be358d221316c191ddfdd96fe
leonthuongto <leon@ixswap.io>
2025-04-17 11:01:48 +0700

    Update subgraph link

--- src/lib/config/base-sepolia/index.ts
+++ src/lib/config/base-sepolia/index.ts
@@ -28,9 +28,9 @@ const config: Config = {
   subgraph: 'https://api.studio.thegraph.com/query/27946/v2-basesepolia/version/latest',
   poolsUrlV2: '',
   subgraphs: {
-    main: ['https://api.studio.thegraph.com/query/80624/dex-reward-v2-basesepolia/version/latest'],
+    main: ['https://api.studio.thegraph.com/query/27946/v2-basesepolia/version/latest'],
     aave: '',
-    gauge: 'https://api.studio.thegraph.com/query/80624/dex-reward-v2-basesepolia/version/latest',
+    gauge: 'https://api.studio.thegraph.com/query/27946/v2-basesepolia/version/latest',
     blocks: '',
   },
   bridgeUrl: '',
