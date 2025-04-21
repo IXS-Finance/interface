33053be1d66c3c465535d1eed214bb58be88eeba
Lê Viết Hưng <hung@ixswap.io>
2025-04-21 11:52:39 +0700

    feat: [IXSPD1-2592] Smart contract integration for Voting Rewards (#3182)

--- src/pages/DexV2/Dashboard/components/VotingRewards.tsx
+++ src/pages/DexV2/Dashboard/components/VotingRewards.tsx
@@ -1,93 +1,204 @@
-import { Box, Button, Chip, Grid, Stack, Tooltip } from "@mui/material"
-import { TYPE } from "theme"
-import styled, { useTheme } from "styled-components"
-import { Card } from "./Card"
+import React, { useMemo } from 'react'
+import { Box, Chip, Grid, Stack, Tooltip } from '@mui/material'
+import { TYPE } from 'theme'
+import styled from 'styled-components'
+import { Card } from './Card'
 import { ReactComponent as InfoIcon } from 'assets/images/info.svg'
 import { ReactComponent as UnlockIcon } from 'assets/images/dex-v2/unlock.svg'
-import { useCurrency } from "hooks/Tokens"
-import DoubleCurrencyLogo from "components/DoubleLogo"
-import CurrencyLogo from "components/CurrencyLogo"
+import useLocksQuery from 'hooks/dex-v2/queries/useLocksQuery'
+import useWeb3 from 'hooks/dex-v2/useWeb3'
+import { BigNumber, utils } from 'ethers'
+import useVotingQuery from 'hooks/dex-v2/queries/useVotingQuery'
+import { Address } from 'viem'
+import CurrencyLogoSet from 'components/CurrencyLogoSet'
+import { useTokens } from 'state/dexV2/tokens/hooks/useTokens'
+import useFlattenedLocks from 'hooks/dex-v2/queries/useFlattenedLocks'
+import { LockedData } from 'services/balancer/contracts/ve-sugar'
+import { PinnedContentButton } from 'components/Button'
 
 const VotingRewards = () => {
   return (
     <Box>
-      <Stack mb={3} direction='row' alignItems='center' gap={1}>
-        <TYPE.label>Voting Rewards</TYPE.label> 
+      <Stack mb={3} direction="row" alignItems="center" gap={1}>
+        <TYPE.label>Voting Rewards</TYPE.label>
         <Tooltip title="Info">
           <InfoIcon />
         </Tooltip>
       </Stack>
       <Box mb={1}>
-        <Card>
-          <TableBody />
-        </Card>
+        <TableBody />
       </Box>
     </Box>
   )
 }
 
+type VotePool = {
+  lp: Address
+  weight: BigNumber
+}
+
+type VoteItem = {
+  id: string
+  rewardToken: Address
+  pools: VotePool[]
+}
+
 const TableBody = () => {
-  const theme = useTheme()
-  const currency0 = useCurrency('0x949546713004ee02537292b1F41046f705909191')
-  const currency1 = useCurrency('0xA9c2c7D5E9bdA19bF9728384FFD3cF71Ada5dfcB')
+  const { account } = useWeb3()
+  const { lockRewards } = useLocksQuery(account)
+  const { flattenedLocks } = useFlattenedLocks(account)
+
+  const lockHasVotes = useMemo(() => {
+    if (!lockRewards) return []
+
+    return lockRewards
+      .filter((reward) => reward.votes.length > 0)
+      .map((reward) => ({
+        id: reward.id,
+        rewardToken: reward.token,
+        pools: reward.votes,
+      }))
+  }, [lockRewards])
 
   return (
-    <Grid container spacing={1}>
-      <Grid item xs={3} style={{ borderRight: `1px solid ${theme.launchpad.colors.border.default}` }}>
-        <Stack direction='row' gap={4}>
-          <Box width={40}>
-            {currency0 && currency1 ? <DoubleCurrencyLogo currency0={currency0} currency1={currency1} size={32} /> : null}
-          </Box>
-          <Box>
-            <TYPE.label fontSize={16}>veIXS/USDT</TYPE.label>
-            <Stack direction='row' alignItems='center' gap={1}>
-              <TYPE.subHeader1 color='yellow69'>Basic Volatile</TYPE.subHeader1>
-              <Dot />
-              <Tooltip title="Info">
-                <InfoIcon width={12} />
-              </Tooltip>
-            </Stack>
-          </Box>
-        </Stack>
-        <TYPE.subHeader1>on OP Mainnet</TYPE.subHeader1>
-      </Grid>
-      <Grid item xs={6}>
-        <Stack direction='row' gap={1} mb={1}>
-          <TYPE.label fontSize={16}>Lock #27216</TYPE.label>
-          <UnlockIcon />
-        </Stack>
-        <TYPE.label fontSize={14} color='text6'>1.55718 IXS locked</TYPE.label>
-      </Grid>
-      <Grid item xs={3}>
-        <Stack gap={0.5}>
-          <Stack direction='row' alignItems='center' justifyContent='flex-end' gap={1}>
-            <CurrencyLogo currency={currency0} size='16px' />
-            <TYPE.black fontSize={14}>0.00013 IXS</TYPE.black>
-            <Chip size='small' label="Fee" />
-          </Stack>
-          <Stack direction='row' alignItems='center' justifyContent='flex-end' gap={1}>
-            <CurrencyLogo currency={currency1} size='16px' />
-            <TYPE.black fontSize={14}>0.00002 USDC</TYPE.black>
-            <Chip size='small' label="Fee" />
-          </Stack>
-          <Stack direction='row' alignItems='center' justifyContent='flex-end' gap={1}>
-            <CurrencyLogo currency={currency0} size='16px' />
-            <TYPE.black fontSize={14}>0.00006 IXS</TYPE.black>
-            <Chip size='small' label="Incentive" />
-          </Stack>
-          <Stack direction='row' alignItems='center' justifyContent='flex-end' gap={1}>
-            <CurrencyLogo currency={currency1} size='16px' />
-            <TYPE.black fontSize={14}>0.0 USDC</TYPE.black>
-            <Chip size='small' label="Incentive" />
-          </Stack>
-          <Stack direction='row' justifyContent='flex-end'>
-            <Button style={{ textTransform: 'none' }}>
-              Claim
-            </Button>
-          </Stack>
-        </Stack>
-      </Grid>
+    <Grid container direction="column" spacing={0.5}>
+      {lockHasVotes?.map((votedLockReward) => (
+        <VotingRewardPerVote
+          key={votedLockReward.id}
+          votedLockReward={votedLockReward}
+          lockData={flattenedLocks[votedLockReward.id]}
+        />
+      ))}
     </Grid>
+  )
+}
+
+type FeeAndBribeRewardPerRow = {
+  tokens: Address[]
+  fees: BigNumber[]
+  bribes: BigNumber[]
+}
+
+const VotingRewardPerVote = ({ votedLockReward, lockData }: { votedLockReward: VoteItem; lockData: LockedData }) => {
+  const pools = votedLockReward.pools
+  const poolAddresses = pools.map((pool) => pool.lp)
+
+  const { data: votingRewardsPerPool } = useVotingQuery(poolAddresses, votedLockReward.id)
+
+  return (
+    <>
+      {pools?.map((poolAddress, i) => {
+        const votingReward = {
+          tokens: votingRewardsPerPool?.tokens[i],
+          fees: votingRewardsPerPool?.feeRewards[i],
+          bribes: votingRewardsPerPool?.bribeRewards[i],
+        } as FeeAndBribeRewardPerRow
+        return (
+          <Grid item key={`vote-${votedLockReward.id}-pool-${poolAddress}`}>
+            <VotingRewardRow votingReward={votingReward} lockData={lockData} />
+          </Grid>
+        )
+      })}
+    </>
+  )
+}
+
+const VotingRewardRow = ({
+  votingReward,
+  lockData,
+}: {
+  votingReward: FeeAndBribeRewardPerRow
+  lockData: LockedData
+}) => {
+  const { getToken } = useTokens()
+  const inputTokenAddresses = votingReward?.tokens
+  const inputTokens = inputTokenAddresses?.map((tokenAddress) => getToken(tokenAddress))
+  const pairName = inputTokens?.map((token) => token.symbol).join('/')
+
+  return (
+    <Card>
+      <Grid container spacing={1}>
+        <Grid item xs={3}>
+          <Stack direction="row" gap={4}>
+            <Box width={40}>
+              <CurrencyLogoSet tokens={inputTokenAddresses} size={36} margin={false} />
+            </Box>
+            <Box>
+              <TYPE.label fontSize={16}>{pairName}</TYPE.label>
+              <Stack direction="row" alignItems="center" gap={1}>
+                <TYPE.subHeader1 color="yellow69">Weighted</TYPE.subHeader1>
+                <Dot />
+                <Tooltip title="Info">
+                  <InfoIcon width={12} />
+                </Tooltip>
+              </Stack>
+            </Box>
+          </Stack>
+        </Grid>
+        <Grid item xs={6}>
+          <Stack direction="row" gap={1} mb={1}>
+            <TYPE.black fontSize={14}>Lock #{lockData.id}</TYPE.black>
+            <UnlockIcon />
+          </Stack>
+          <TYPE.black fontSize={14} color="">
+            {lockData.amount} IXS locked
+          </TYPE.black>
+        </Grid>
+        <Grid item xs={3}>
+          <Stack direction="row" gap={2} alignItems="start" justifyContent="flex-end">
+            <Stack gap={0.5}>
+              {inputTokenAddresses?.map((tokenAddress, i) => {
+                const token = getToken(tokenAddress)
+                return (
+                  <Stack
+                    key={`fee-${tokenAddress}`}
+                    direction="row"
+                    alignItems="center"
+                    justifyContent="flex-end"
+                    gap={1}
+                  >
+                    <TYPE.black fontSize={14}>
+                      {utils.formatUnits(votingReward.fees[i], token.decimals)}{' '}
+                      <FeeAndBribeRewardTokenSymbol>{token.symbol}</FeeAndBribeRewardTokenSymbol>
+                    </TYPE.black>
+                    <FeeAndBribeRewardLabel size="small" label="Fee" variant="outlined" />
+                  </Stack>
+                )
+              })}
+              {inputTokenAddresses?.map((tokenAddress, i) => {
+                const token = getToken(tokenAddress)
+                return (
+                  <Stack
+                    key={`incentive-${tokenAddress}`}
+                    direction="row"
+                    alignItems="center"
+                    justifyContent="flex-end"
+                    gap={1}
+                  >
+                    <TYPE.black fontSize={14}>
+                      {utils.formatUnits(votingReward.bribes[i], token.decimals)}{' '}
+                      <FeeAndBribeRewardTokenSymbol>{token.symbol}</FeeAndBribeRewardTokenSymbol>
+                    </TYPE.black>
+                    <FeeAndBribeRewardLabel size="small" label="Incentive" variant="outlined" />
+                  </Stack>
+                )
+              })}
+            </Stack>
+
+            <PinnedContentButton
+              style={{
+                width: 'auto',
+                paddingTop: 12,
+                paddingBottom: 12,
+              }}
+              onClick={() => {}}
+            >
+              Claim
+            </PinnedContentButton>
+          </Stack>
+        </Grid>
+      </Grid>
+    </Card>
   )
 }
 
@@ -96,6 +207,16 @@ const Dot = styled.div`
   height: 2px;
   border-radius: 50%;
   background: ${({ theme }) => theme.text6};
+`
+
+const FeeAndBribeRewardLabel = styled(Chip)`
+  border: 1px solid ${({ theme }) => theme.bg24} !important;
+  span {
+    color: ${({ theme }) => theme.text6};
+  }
+`
+const FeeAndBribeRewardTokenSymbol = styled.span`
+  color: ${({ theme }) => theme.text6};
 `
 
 export default VotingRewards
