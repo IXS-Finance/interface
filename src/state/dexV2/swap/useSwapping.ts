f908212a43f184eef6d18b58164512af92be472b
leonthuongto <leon@ixswap.io>
2025-04-23 17:01:26 +0700

    Handle mistake for handleAmountChange when change tokenOutAddressInput (#3195)

--- src/state/dexV2/swap/useSwapping.ts
+++ src/state/dexV2/swap/useSwapping.ts
@@ -164,7 +164,7 @@ export default function useSwapping(
 
   useEffect(() => {
     handleAmountChange()
-  }, [tokenInAddressInput, tokenInAmountInput])
+  }, [tokenInAddressInput, tokenOutAddressInput])
 
   useEffect(() => {
     handleAmountChange()
