031caea7b41ed5e4a1cb4c60c78df584d7edba3f
Lê Viết Hưng <hung@ixswap.io>
2025-04-24 10:47:11 +0700

    fix: [IXSPD1-2611] avoid extend with time less than current locked time (#3196)

--- src/pages/DexV2/Lock/Detail/components/LockExtendDurationSlider.tsx
+++ src/pages/DexV2/Lock/Detail/components/LockExtendDurationSlider.tsx
@@ -1,48 +1,71 @@
-import React from "react"
-import { Slider } from "@mui/material"
-import { Box, Flex } from "rebass"
-import styled from "styled-components"
-import { TYPE } from "theme"
-import { FOUR_YEARS_IN_SECONDS, WEEK } from "../../constants"
-import { formatAmount } from "utils/formatCurrencyAmount"
-import { useLockDetail } from "../LockDetailProvider"
+import React, { useEffect } from 'react'
+import { Slider } from '@mui/material'
+import { Box, Flex } from 'rebass'
+import styled from 'styled-components'
+import { TYPE } from 'theme'
+import { useParams } from 'react-router-dom'
+
+import { FOUR_YEARS_IN_SECONDS, WEEK } from '../../constants'
+import { formatAmount } from 'utils/formatCurrencyAmount'
+import { useLockDetail } from '../LockDetailProvider'
+import useLockQuery from 'hooks/dex-v2/queries/useLockQuery'
 
 const LockExtendDurationSlider: React.FC = () => {
-  const {
-    userInput,
-    duration,
-    setDuration,
-  } = useLockDetail()
+  const params = useParams<{ id: string }>()
+  const lockId = params.id.toLowerCase()
+  const { userInput, duration, setDuration } = useLockDetail()
+  const { lockDetail } = useLockQuery(lockId)
+
+  const currentTime = Math.floor(Date.now() / 1000)
+  const defaultLockTime = lockDetail?.expiresAt ? +lockDetail.expiresAt - currentTime + WEEK : WEEK
+
+  useEffect(() => {
+    if (lockDetail?.expiresAt) {
+      setDuration(+lockDetail.expiresAt - currentTime)
+    }
+  }, [lockDetail?.expiresAt])
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setDuration(Number(e.target?.value));
-  };
+    if (!e.target?.value) return
+    if (+e.target.value < defaultLockTime) {
+      setDuration(defaultLockTime)
+      return
+    }
 
-  const rangeSelectors = [{
-    label: '7 Days',
-    value: 604800,
-  }, {
-    label: '1 Year',
-    value: 31536000,
-  }, {
-    label: '2 Years',
-    value: 63072000,
-  }, {
-    label: '3 Years',
-    value: 94608000,
-  }, {
-    label: '4 Years',
-    value: 126230400,
-  }, ]
+    setDuration(Number(e.target?.value))
+  }
+
+  const rangeSelectors = [
+    {
+      label: '7 Days',
+      value: WEEK,
+    },
+    {
+      label: '1 Year',
+      value: 31536000,
+    },
+    {
+      label: '2 Years',
+      value: 63072000,
+    },
+    {
+      label: '3 Years',
+      value: 94608000,
+    },
+    {
+      label: '4 Years',
+      value: 126230400,
+    },
+  ]
 
   const weekToShow = Math.round(duration / WEEK)
   const durationLabel = `${weekToShow} ${weekToShow > 1 ? 'weeks' : 'week'}`
-  const votingPower = +userInput * duration / FOUR_YEARS_IN_SECONDS
+  const votingPower = (+userInput * duration) / FOUR_YEARS_IN_SECONDS
 
   return (
     <Box>
       <Flex justifyContent="space-between" alignItems="center" mb={2}>
-        <TYPE.subHeader1 color='blue5'>Lock Time</TYPE.subHeader1>
+        <TYPE.subHeader1 color="blue5">Lock Time</TYPE.subHeader1>
         <TYPE.label>{durationLabel}</TYPE.label>
       </Flex>
 
@@ -52,10 +75,10 @@ const LockExtendDurationSlider: React.FC = () => {
           color: '#6666FF',
         }}
         value={duration}
-        onChange={e => handleChange(e as unknown as React.ChangeEvent<HTMLInputElement>)}
-        step={604800} // 7 days
-        min={604800} // 7 days
-        max={126230400} // 4 years
+        onChange={(e) => handleChange(e as unknown as React.ChangeEvent<HTMLInputElement>)}
+        step={WEEK} // 7 days
+        min={WEEK}
+        max={FOUR_YEARS_IN_SECONDS}
       />
 
       <Flex justifyContent="space-between" mt={2}>
