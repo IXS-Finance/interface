2909a5bac98812ea51ef1e5285822e94877dfd3e
Lê Viết Hưng <hung@ixswap.io>
2025-04-28 16:59:04 +0700

    feat: [IXSPD1-2638] claim voting rewards (#3216)

--- src/pages/DexV2/Pool/List/hooks.ts
+++ src/pages/DexV2/Pool/List/hooks.ts
@@ -1,9 +1,5 @@
-import { useCallback, useMemo } from "react"
-import { AbstractOrder } from "state/launchpad/types"
-import { useSubgraphQueryLegacy } from "hooks/useSubgraphQuery";
-import { SUBGRAPH_QUERY } from "constants/subgraph";
-import { usePoolFilter } from "./FilterProvider";
-import { adminOffset } from "state/admin/constants";
+import { useCallback } from 'react'
+import { AbstractOrder } from 'state/launchpad/types'
 
 export const useOnChangeOrder = (
   order: AbstractOrder,
@@ -28,59 +24,4 @@ export const useOnChangeOrder = (
   )
 
   return onChangeOrder
-}
-
-export enum PageModal {
-  NETWORK_SELECTOR,
-}
-
-export const usePoolList = () => {
-  const { order, filters, page } = usePoolFilter()
-
-  const orderBy = Object.keys(order)[0]
-  const orderDirection = Object.values(order)[0]?.toLowerCase()
-
-  const variables = useMemo(() => ({
-    orderBy,
-    orderDirection,
-    first: adminOffset, // equal to pageSize
-    skip: page * adminOffset,
-  }), [orderBy, orderDirection, adminOffset, page])
-
-  const subgraphData = useSubgraphQueryLegacy({
-    feature: SUBGRAPH_QUERY.POOLS,
-    chainId: filters.network,
-    query: `
-      query GetPools(
-        $orderBy: String,
-        $orderDirection: String,
-        $skip: Int,
-        $first: Int,
-      ) {
-        pools(
-          orderBy: $orderBy,
-          orderDirection: $orderDirection,
-          skip: $skip,
-          first: $first,
-        ) {
-          address
-          id
-          totalSwapVolume
-          totalLiquidity
-          tokens {
-            address
-            symbol
-            weight
-          }
-        }
-      }
-    `,
-    variables,
-    autoPolling: true,
-    pollingInterval: 20_000, // 20 seconds
-  })
-
-  return {
-    pools: subgraphData?.pools,
-  }
 }
