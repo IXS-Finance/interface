2bdf4b692f06bbefc960c8ceffae4051854a9851
leonthuongto <leon@ixswap.io>
2025-04-28 22:15:52 +0700

    Should be able to vote after new epoch starts (#3218)

--- src/pages/DexV2/Vote/index.tsx
+++ src/pages/DexV2/Vote/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Flex } from 'rebass'
+import _get from 'lodash/get'
 
 import VotingRoundStats from './components/VotingRoundStats'
 import { LiquidityPoolSelector } from './components/LiquidityPoolSelector'
@@ -7,21 +8,26 @@ import usePoolsHasGaugeQuery from 'hooks/dex-v2/queries/usePoolsHasGaugeQuery'
 import DexV2Layout from '../common/Layout'
 import SelectLockToVote from './components/SelectLockToVote'
 import { VoteHistoryRecords } from './components/VoteHistoryRecords'
+import useVoteInfoQuery from 'hooks/dex-v2/queries/useVoteInfoQuery'
 
 interface VoteProps {}
 
 const Vote: React.FC<VoteProps> = () => {
-  const {
-    data: poolsData
-  } = usePoolsHasGaugeQuery()
+  const query = useVoteInfoQuery()
+
+  const { data: poolsData } = usePoolsHasGaugeQuery()
 
   const pools = poolsData?.pools || []
+  const epochVoteEnd = _get(query, 'data.epochVoteEnd', null)
+  const epochVoteStart = _get(query, 'data.epochVoteStart', null)
+  const totalSupply = _get(query, 'data.totalSupply', null)
+  const availableDeposit = _get(query, 'data.availableDeposit', null)
 
   return (
     <DexV2Layout>
       <Flex flexDirection="column" css={{ gap: '48px', width: '100%' }}>
-        <VotingRoundStats />
-        <SelectLockToVote pools={pools} />
+        <VotingRoundStats epochVoteEnd={epochVoteEnd} totalSupply={totalSupply} availableDeposit={availableDeposit} />
+        <SelectLockToVote pools={pools} epochVoteStart={epochVoteStart} epochVoteEnd={epochVoteEnd} />
         <LiquidityPoolSelector pools={pools} />
         <VoteHistoryRecords pools={pools} />
       </Flex>
