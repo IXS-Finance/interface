fb3546fa16931a16315c3cd7ad8a553206c9e667
Hung Le <hung@ixswap.io>
2025-05-20 08:24:05 +0700

    feat: add useGauges function and obtain data from subgraph

---
+++ src/services/balancer/gauges/entities/gauges/gauges.entity.ts
@@ -0,0 +1,18 @@
+import { SubgraphGauge } from 'services/dexV2/gauges/types'
+import { GaugesSubgraphService } from '../../gauges-subgraph.service'
+import { QueryBuilder } from '../../types'
+import { gaugeQueryBuilder } from './query'
+
+export class GaugesEntity {
+  constructor(
+    private readonly service: GaugesSubgraphService,
+    private readonly query: QueryBuilder = gaugeQueryBuilder
+  ) {}
+
+  public async get(args = {}, attrs = {}): Promise<SubgraphGauge[]> {
+    const queryName = 'Gauges'
+    const query = this.query(args, attrs, queryName)
+    const data = await this.service.client.get(query)
+    return data.gauges
+  }
+}
